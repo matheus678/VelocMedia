ad37f46c82b98f9be16438f09016d5b798c34684
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-22 02:07:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Você pecisa calcular a velocidade média?");
+
+            Console.WriteLine(" pressione enter para continuar...:");
+
+            Console.ReadKey();
+
+
+
+            Console.WriteLine("Informe a distância em metros (M) percorrida.");
+
+            double espaço = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Informe o tempo em segundos (S) que foi gasto.");
+
+            double tempo = double.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Sua velocidade média é: ");
+
+            double resultado = (espaço / tempo);
+
+            Console.Write( resultado);
+
+            Console.WriteLine(" m/s.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
